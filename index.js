96dc1f595603b7091e7399f8f48e78a1471187d7
Sharon Byegon <byegon.sharon@gmail.com>
2024-03-28 07:29:25 +0300

    updated

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// remove the DOM node 'main#main'
+document.querySelector('main#main').remove();
+
+// create a newHeader variable that points to an <h1> node
+let newHeader = document.createElement('h1');
+
+// assign an id of 'victory' to the newHeader variable
+newHeader.id = 'victory';
+
+// assign the text "YOUR-NAME is the champion" to the newHeader variable
+newHeader.innerHTML = "Sharon is the champion";
